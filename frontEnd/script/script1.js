aebdb6a234f0228ba7cec4a9c4ecf8689412bb11
JasonLouw <mjasonlouw@gmail.com>
2019-03-28 01:25:25 +0200

    merge front and backend to database

--- frontEnd/script/script1.js
+++ frontEnd/script/script1.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    console.log("jquery working..");
+
     function request(url, startDate1, endDate1)
     {
         $.get( url, {startDate:startDate1,endDate:endDate1}, 
@@ -11,8 +13,53 @@ $(document).ready(function(){
             });
     }
 
+    function convert(uni)
+    {
+        var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var date = new Date(uni);
+        var year = date.getFullYear();
+        var month = months_arr[date.getMonth()];
+        var day = date.getDate();
+        var hours = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+        // Display date time in MM-dd-yyyy h:m:s format
+        var convdataTime = month+' '+day+' '+year;//+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        
+        return convdataTime;
+       }
+
+
+    //slider bar
+
+    var start = 1546300800000;
+    var end = Date.now();
+    $( "#slider-range" ).slider({
+
+        range: true,
+        min: 1546300800000,
+        max: end,
+        values: [ 1546300800000, end ],
+        slide: function( event, ui ) {
+          $( "#amount" ).val(  convert(ui.values[ 0 ])  );
+          $( "#amount1" ).val( convert(ui.values[ 1 ]) );
+          start = ui.values[ 0 ];
+          end = ui.values[ 1 ];
+        },
+        stop: function(event, ui)
+        {
+            changing();
+        }
+      });
+
+    
+        $( "#amount" ).val( convert(1546300800000));
+        $( "#amount1" ).val( convert(end));
+        
+
     function OTP()
     {
+        //OTP table generator inserts the html table in the div with the class name info/
         var table = '<table class="table"><thead><th>clientID</th><th>OTP</th><th>timeStamp</th><th>success</th></thead><tbody>';
         var obj;
 
@@ -34,18 +81,18 @@ $(document).ready(function(){
 
     function ClientInformation()
     {
+        //Client information table generator inserts the html table in the div with the class name info.
         var tableDat = '<table class="table"><thead><th>timestamp</th><th>clientID</th><th>code</th><th>description</th></thead><tbody>';
         var obj;
-        
-
-        $.getJSON("tempJson/ClientNotification.json", function(data){
+    
+        $.getJSON("tempJson/ClientNotification_data.json", function(data){
             for(i in data)
             {
                 tableDat+= '<tr>';
-                    table += '<td>' + data[i]['timestamp'] + '</td>';
-                    table += '<td>' + data[i]['clientID'] + '</td>';
-                    table += '<td>' + data[i]['code'] + '</td>';
-                    table += '<td>' + data[i]['description'] + '</td>';
+                tableDat += '<td>' + data[i]['timestamp'] + '</td>';
+                tableDat += '<td>' + data[i]['clientID'] + '</td>';
+                tableDat += '<td>' + data[i]['code'] + '</td>';
+                tableDat += '<td>' + data[i]['description'] + '</td>';
                 tableDat += '</tr>'
             }
             tableDat += "</tbody></table>";
@@ -55,6 +102,7 @@ $(document).ready(function(){
 
  function NFC()
  {
+     //NFC table generator inserts the html table in the div with the class name info.
     var table = '<table class="table"><thead><th>Date</th><th>StatusCode</th><th>method</th><th>url</th><th>cardId</th><th>ip</th></thead><tbody>';
     var obj;
 
@@ -78,45 +126,117 @@ $(document).ready(function(){
     });        
 }
 
-    $(".submitBut").click(function(){
-        $(".info").html("");
-        var module = $(".moduleName").val();
-        var startDate = $(".startDate").val();
-        var endDate = $(".endDate").val();
+function changing(){
+    $(".info").html("");
+    var module = $(".moduleName").val();
+    var startDate = start;
+    var endDate = end;
+    console.log("module: "+module);
+    console.log("start date: "+startDate);
+    console.log("end date: "+endDate);
+    if(module == null)
+    {
+        console.log("Please select a module: "+endDate);
+        return;
+    }
 
-        console.log(" submit workking"+module+startDate+endDate);
-      
-        if(module == "ATM Simulation")
-        {
-           
-        }
-        if(module == "Authenticaion")
-        {
-           
-        }
-        if(module == "Facial Recognition")
-        {
-            
-        }
-        if(module == "NFC")
-        {
-            NFC();
-        }
-        if(module == "OTP")
-        {
-            OTP()
-        }
-        if(module == "Client Information System")
-        {
-            
-        }
-        if(module == "Client Accounts System")
-        {
-            
-        }
-        if(module == "Client Notification")
-        {
-            ClientInformation();
-        }
-    });
+    $(".firstImage").hide();
+    if(module == "ATM Simulation")
+    {
+       //todo: make ATM simulation table
+    }
+    if(module == "Authenticaion")
+    {
+       //todo: make Authenticaion simulation table
+    }
+    if(module == "Facial Recognition")
+    {
+        //todo: make Facial Recognition simulation table
+    }
+    if(module == "NFC")
+    {
+    
+        $("<button value='submit' class='downloadPDF' type='button'>Download PDF</button>" ).insertBefore( ".info" );
+        NFC();
+        $(".downloadPDF").click(function(){
+            // console.log("print pdf");
+            // table.find('tr').each(function (i, el) {
+            //     var $tds = $(this).find('td');
+            //         var x1 = $tds.eq(0).text();
+            //         var x2 = $tds.eq(1).text();
+            //         var x3 = $tds.eq(2).text();
+            //         console.log(x1+x2+x3);
+            //     // do something with productId, product, Quantity
+            // });
+            var placement = 80;
+            var one = false;
+            var doc = new jsPDF();
+            var imgData = '';
+            doc.addImage(imgData, 'JPEG', 15, 15, 90, 38); doc.text("NFC Report",15,70); $("table tr").each(function(index) { //makes pdf for NFC 
+                var $tds = $(this).find('td'); 
+                var x1 = $tds.eq(0).text(); 
+                var x2 = $tds.eq(1).text(); 
+                var x3 = $tds.eq(2).text(); 
+                var x4 = $tds.eq(3).text(); 
+                var x5 = $tds.eq(4).text(); 
+                var x6 = $tds.eq(5).text(); 
+                console.log(x1+" "+x2+" "+x3+" "+x4+" "+x5+" "+x6); 
+                if(one)
+                        {
+                            if(x1 != "")
+                            {
+                                var date = "Date: "+x1;
+                                var status = " Status Code: "+x2;
+                                var method = " Method: "+x3;
+                                var url = " url: "+x4;
+                                var card = " Card ID: "+x5;
+                                var ip = " IP: "+x6;
+                                doc.setFontSize(8);
+                                doc.text(date+status+method+url+card+ip,15,placement);
+                                placement = placement + 5;
+                            }
+                        // doc.text(status,20,placement);
+                        // placement = placement + 10;
+                        // doc.text(method,20,placement);
+                        // placement = placement + 10;
+                        // doc.text(url,20,placement);
+                        // placement = placement + 10;
+                        // doc.text(card,20,placement);
+                        // placement = placement + 10;
+                        // doc.text(ip,20,placement);
+                        // placement = placement + 10;
+                        // doc.text(" ",20,placement);
+                        }
+                        one = true;
+
+                    
+            });
+            doc.save('nfcReport.pdf')
+        });
+    }
+    if(module == "OTP")
+    {
+        OTP()
+    }
+    if(module == "Client Information System")
+    {
+        //TODO: make client information table
+    }
+    if(module == "Client Accounts System")
+    {
+         //TODO: Client Accounts System
+    }
+    if(module == "Client Notification")
+    {
+        ClientInformation();
+    }
+}
+
+
+    $(".moduleName").change(function(){
+        changing();
+    } );
+
+
+  
 });
--- index.js
+++ index.js
@@ -1,91 +1,110 @@
+//nodemon to automatically restart server on changes
 var express = require('express');
+//var mongo = require('mongodb').MongoClient;
+var cors = require('cors');
+var $ = require('jQuery');
 var app = express();
+var database = require('./databaseOperations.js');
+
+
+
 var bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-/*view egine setup
+/*view engine setup
     app.set must come after all dependencies and constants defined
     and before the first use of app.use
 */
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
-
 /*  
     im assuming this includes the folder your wana use
     so that you have to always type out the full file
     paths or something 
 */
-app.use(express.static('frontEnd'));
 
-app.get('/', function (req, res) {//front end
-    res.sendfile('index.html');
-});
+    app.use(express.static('frontEnd'));
+    app.use(express.static('testPost'));
+   
+     //Write databse Insert/Update/Query code here..
+                
 
-app.post('/api', function (req, res) {//where people will post their json to
-    var system = req.body.system;
-    var data = req.body.data;
+    app.get('/', function (req, res) {//front end
+        console.log("here");
+        res.sendfile('/index.html');
+    });
 
-    if (req.body.system !== undefined && req.body.data !== undefined) 
-    {
-        console.log("system defined");
-        if(system == "ATMSS")//atm simulation
+    app.post('/api', function (req, res) {//where people will post their json to
+        res.header("Access-Control-Allow-Origin", "*");
+        var system = req.body.system;
+        var data = req.body.data;
+        console.log(data);
+
+        if (req.body.system !== undefined && req.body.data !== undefined) 
         {
-    
-        }
-        else if(system == "AUTH")//Authentication
-        {
-    
-        }
-        else if(system == "CRDS")//Card authentication
-        {
-    
-        }
-        else if(system == "OTPS")//OTP authentication
-        {
-    
-        }
-        else if(system == "FRS")//Facial recognition
-        {
-    
-        }
-        else if(system == "CIS")//client information
-        {
-    
-        }
-        else if(system == "CAS")//Client Accounts
-        {
-    
-        }
-        else if(system == "NS")//Notifications
-        {
-    
-        }
-        else if(system == "REP")//Reporting
-        {
-            //this is our subsystem
+            console.log("system defined");
+            if(system == "ATMSS")//atm simulation
+            {
+                var jo = JSON.parse(data); 
+                database.insert("ATMSS",jo);
+                var fetched = database.fetch("ATMSS");
+                console.log("fetched: "+JSON.stringify(fetched)+"-end");
+            }
+            else if(system == "AUTH")//Authentication
+            {
+         
+            }
+            else if(system == "CRDS")//Card authentication
+            {
+               
+            }
+            else if(system == "OTPS")//OTP authentication
+            {
+                
+            }
+            else if(system == "FRS")//Facial recognition
+            {
+                
+            }
+            else if(system == "CIS")//client information
+            {
+                
+            }
+            else if(system == "CAS")//Client Accounts
+            {
+               
+            }
+            else if(system == "NS")//Notifications
+            {
+                
+            }
+            else if(system == "REP")//Reportings
+
+            {
+                //this is our subsystem
+            }
+            else
+            {
+                res.write('{"ERROR":"correct parameters, wrong subsysetm"}')
+            }
         }
         else
         {
-    
+            res.write('{"ERROR":"correct parameters or Not url encoded"}')
         }
-    }
-    else
-    {
-        
-    }
+        res.end();
+    });
 
-   
+    app.listen(process.env.PORT || 4004, function(){ //must be at end of js file
+        console.log("Listening on port 4004");
+        //process.exit();
+    });
 
-    console.log(system);
-    console.log(data);
-    console.log("post check");
 
-    res.end();
-});
+//express.on('SIGINT');
 
-app.listen(8080, function(){ //must be at end of js file
-    console.log("Listening on port 8080!");
-});
+//process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); });
